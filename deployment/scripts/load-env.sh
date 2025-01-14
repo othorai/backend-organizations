1e220588e5685624e47c1f6f8d92f6561c312596
Nekender Shekhawat <108869998+nekender@users.noreply.github.com>
2025-01-14 06:02:15 +0530

    Deployment fix

--- deployment/scripts/load-env.sh
+++ deployment/scripts/load-env.sh
@@ -3,8 +3,8 @@
 
 # Function to load .env file
 load_env() {
-    if [ -f .env ]; then
-        export $(cat .env | sed 's/#.*//g' | xargs)
+    if [ -f ../../.env ]; then
+        export $(cat ../../.env | sed 's/#.*//g' | xargs)
     else
         echo ".env file not found"
         exit 1
